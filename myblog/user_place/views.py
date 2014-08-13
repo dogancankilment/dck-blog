a965bb31f2c13b06af8bf6f3151bb1bed1917155
dogancankilment <dogancankilment@gmail.com>
2014-08-13 10:58:50 +0300

    added token_generator.py import line

--- myblog/user_place/views.py
+++ myblog/user_place/views.py
@@ -2,46 +2,16 @@ from django.shortcuts import render, redirect, render_to_response, get_object_or
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required, permission_required
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import get_template
-from django.template import Context
-from django.template.loader import render_to_string
 from forms import *
 from django.core.context_processors import csrf
 from django.contrib import auth
 from django.contrib import messages
 from django.utils.translation import ugettext as _
 from main_site.views import index
-from user_place.utils import *
+from user_place.mail_sender import mail_sender
+from token_generator import *
 
 import datetime
-
-
-def mail_sender(request, email):
-        plaintext = get_template('email/email_content.html')
-        subject, from_email = 'hello',\
-                              'surveydck@gmail.com'
-        if email:
-            to = email
-
-            hash_key_example = activation_key_generator(to)
-            transmitted_key = Context(
-                {'hash_key': hash_key_example})
-
-            # text_content = plaintext.render(d)
-            text_content = render_to_string('email/email_content.html',
-                                            transmitted_key)
-
-            msg = EmailMultiAlternatives(subject,
-                                         text_content,
-                                         from_email,
-                                         [to])
-            msg.send()
-            return HttpResponse(_("mailiniz gonderildi"))
-
-        else:
-            return HttpResponse(_("Gondereceginiz kisinin"
-                                "email adresi belli olmalidir."))
 
 
 @login_required(login_url='/user/login')
