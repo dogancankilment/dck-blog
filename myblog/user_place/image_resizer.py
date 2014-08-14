bb100844fccb3e74068d09b150ec7cb264b87533
dogancankilment <dogancankilment@gmail.com>
2014-08-14 15:14:19 +0300

    running with example image data

--- myblog/user_place/image_resizer.py
+++ myblog/user_place/image_resizer.py
@@ -1,4 +1,11 @@
 import PIL
 from PIL import Image
-import os
-import sys
+
+
+def image_resizer():
+    basewidth = 300
+    img = Image.open('deneme.jpg')
+    wpercent = (basewidth / float(img.size[0]))
+    hsize = int((float(img.size[1]) * float(wpercent)))
+    img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+    img.save('resized_image.jpg')
