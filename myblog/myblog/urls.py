27c90c254d30eb58121721fc6f514eb3ffd0789c
dogancankilment <dogancankilment@gmail.com>
2014-08-17 17:24:17 +0300

    syntax edited for pep8 rules and pep8 error corrected, mail_sender url deleted in user_place/urls.py

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -1,25 +1,23 @@
+# from django.conf import settings
 from django.conf.urls import patterns, include, url
-from django.conf import settings
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
+                       url(r'^$',
+                           'main_site.views.index',
+                           name='index'),
 
-    # url(r'^myblog/', include('myblog.foo.urls')),
+                       url(r'^user/',
+                           include('user_place.urls')),
 
-    url(r'^$',
-        'main_site.views.index',
-        name='index'),
+                       url(r'^blog/',
+                           include('main_site.urls')),
 
-    url(r'^user/',
-        include('user_place.urls')),
-
-    url(r'^blog/',
-        include('main_site.urls')),
-
-    url(r'^admin/',
-        include(admin.site.urls)),
-)
+                       url(r'^admin/',
+                           include(admin.site.urls)),
+                       )
 
 handler404 = 'main_site.views.my_custom_404'
 handler500 = 'main_site.views.my_custom_500'
--- myblog/user_place/urls.py
+++ myblog/user_place/urls.py
@@ -2,29 +2,23 @@ from django.conf.urls import patterns, url
 # from django.conf import settings
 
 urlpatterns = patterns('',
+                       url(r'^test_view$',
+                           'user_place.views.test_view',
+                           name='test_view'),
 
-    url(r'^test_view$',
-        'user_place.views.test_view',
-        name='test_view'),
+                       url(r'^login$',
+                           'user_place.views.my_login',
+                           name='my_login'),
 
-    url(r'^login$',
-        'user_place.views.my_login',
-        name='my_login'),
+                       url(r'^signup$',
+                           'user_place.views.signup',
+                           name='signup'),
 
-    url(r'^signup$',
-        'user_place.views.signup',
-        name='signup'),
+                       url(r'^logout$',
+                           'user_place.views.logout',
+                           name='logout'),
 
-    url(r'^logout$',
-        'user_place.views.logout',
-        name='logout'),
-
-    url(r'^mail_sender$',
-        'user_place.views.mail_sender',
-        name='mail_sender'),
-
-    url(r'^activation/(?P<token_id>.*)',
-        'user_place.views.activation',
-        name='activation'),
-
-)
+                       url(r'^activation/(?P<token_id>.*)',
+                           'user_place.views.activation',
+                           name='activation'),
+                       )
