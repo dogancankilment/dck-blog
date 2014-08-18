25e0a3c8f815edcbc399f830ffa497fd7cbbf0ef
dogancankilment <dogancankilment@gmail.com>
2014-08-18 16:13:56 +0300

    added task config_from_object line

--- myblog/myblog/celery.py
+++ myblog/myblog/celery.py
@@ -7,6 +7,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
 
 app = Celery('myblog', backend='amqp', broker='amqp://')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.config_from_object('django.conf:settings')
 
 
 @app.task(bind=True)
