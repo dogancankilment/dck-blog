0daffa625cab8fd658edfcc1063797d14322e749
dogancankilment <dogancankilment@gmail.com>
2014-08-18 09:12:43 +0300

    django celery installed. celery.py created in main directory. used django-celery documentation

---
+++ myblog/myblog/celery.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+from django.conf import settings
+from celery import Celery
+
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
+
+app = Celery('myblog', backend='amqp', broker='amqp://')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
