50c3ab305242fbca710bb63757a2b9e009f1a2fd
dogancankilment <dogancankilment@gmail.com>
2014-08-19 18:33:49 +0300

    pep8 configured

--- myblog/main_site/urls.py
+++ myblog/main_site/urls.py
@@ -7,8 +7,15 @@ urlpatterns = patterns('',
                            name='main_site.index'),
 
                        url(r'^new_post$',
-                           'main_site.views.new_post'),
+                           'main_site.views.new_post',
+                           name='new_post'),
 
                        url(r'^single_post/(?P<id>\w+)',
-                           'main_site.views.single_post'),
+                           'main_site.views.single_post',
+                            name='single_post'),
+
+                       url(r'^edit_post/(?P<id>\w+)',
+                           'main_site.views.edit_post',
+                            name='edit_post'),
+
 )
