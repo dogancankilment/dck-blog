d264428392f5607ab560fec209ea17067f729a08
dogancankilment <dogancankilment@gmail.com>
2014-08-19 20:40:34 +0300

    Generic ForeignKey used, GenericRelations added

--- myblog/main_site/models.py
+++ myblog/main_site/models.py
@@ -1,18 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, related_name="for_index")
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.generic import GenericForeignKey, GenericRelation
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     which_user = models.ForeignKey(User)
+    # image = models.ImageField()
+
+    comments = GenericRelation('Comments')
 
 
 class Comments(models.Model):
-    content = models.CharField(max_length=200)
     which_user = models.ForeignKey(User)
-    which_post = models.ForeignKey(Post)
+    content = models.CharField(max_length=200)
+
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    parent_object = GenericForeignKey('content_type', 'object_id')
+
+    # comment's comment's comment's comment..
+    comments = GenericRelation('Comments')
