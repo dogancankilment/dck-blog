70efde3183cce4cc41d6e53c1e11fb16275c1a1e
dogancankilment <dogancankilment@gmail.com>
2014-08-19 15:22:51 +0300

    single_post url created for show one post

--- myblog/main_site/urls.py
+++ myblog/main_site/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import patterns, include, url
 # from django.conf import settings
 
 urlpatterns = patterns('',
+                       url(r'^index$',
+                           'main_site.views.index',
+                           name='main_site.index'),
 
-    url(r'^index$',
-        'main_site.views.index',
-        name='main_site.index'),
+                       url(r'^new_post$',
+                           'main_site.views.new_post'),
 
-    url(r'^new_post$',
-        'main_site.views.new_post'),
+                       url(r'^single_post/(?P<id>\w+)',
+                           'main_site.views.single_post'),
 )
