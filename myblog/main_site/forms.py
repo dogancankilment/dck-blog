85f16627391fea92ebcaecfc273e8d133654e67d
dogancankilment <dogancankilment@gmail.com>
2014-08-19 13:10:56 +0300

    form written for add new post

---
+++ myblog/main_site/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.admindocs.tests import fields
+from django.forms import ModelForm
+from .models import Post
+
+
+class New_Post(forms.Form):
+    title = forms.CharField(max_length=100)
+    content = forms.CharField(widget=forms.Textarea)
+    # class Meta:
+    #     model = Post
+    #     # fields= ('title', 'content', 'whi')
