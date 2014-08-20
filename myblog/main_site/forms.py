587a9f42fc4a6f9228eab91d386f50b7f2fa88dc
dogancankilment <dogancankilment@gmail.com>
2014-08-20 16:25:16 +0300

    which user field deleted in form. BaseModelForm save function override

--- myblog/main_site/forms.py
+++ myblog/main_site/forms.py
@@ -7,3 +7,12 @@ from .models import Post
 class New_Post(ModelForm):
     class Meta:
         model = Post
+        exclude = ['which_user']
+
+    def save(self, user):
+        post = Post()
+        post.title = self.cleaned_data.get('title')
+        post.content = self.cleaned_data.get('content')
+        post.which_user = user
+        post.image = self.cleaned_data.get('image')
+        post.save()
