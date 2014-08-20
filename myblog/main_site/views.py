7d13b208f69f7beccb818e60a10b68a27a4fea80
dogancankilment <dogancankilment@gmail.com>
2014-08-20 15:42:57 +0300

    pep8 configured

--- myblog/main_site/views.py
+++ myblog/main_site/views.py
@@ -18,11 +18,14 @@ def index(request):  # blog_id
     comment_list = Comments.objects.all()
     paginator = Paginator(blog_list, 3)
     page = request.GET.get('page')
+
     try:
         post_count = paginator.page(page)
+
     except PageNotAnInteger:
         # If page is not an integer, deliver first page.
         post_count = paginator.page(1)
+
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
         post_count = paginator.page(paginator.num_pages)
@@ -45,16 +48,20 @@ def edit_post(request, id):
     post = Post.objects.get(id=id)
     if request.POST:
         form = New_Post(request.POST)
+
         if form.is_valid():
             post.title = form.cleaned_data.get('title')
             post.content = form.cleaned_data.get('content')
             post.which_user = request.user
             post.save()
+
             return redirect(reverse(index))
+
     else:
         form = New_Post(initial={'title': post.title,
                                  'content': post.content,
                                  'which_user': post.which_user})
+
     c = {"form": form, "id": post.id}
     c.update(csrf(request))
     return render_to_response('post/edit_post.html',c)
@@ -74,7 +81,6 @@ def new_post(request):
 
     else:
         form = New_Post()
-
         c = {"form": form}
         c.update(csrf(request))
 
@@ -83,14 +89,12 @@ def new_post(request):
 
 
 def my_custom_404(request, template_name='404.html'):
-
     #  404 error handler. Templates: `404.html' Context: None
     return render_to_response(template_name,
                               context_instance=RequestContext(request))
 
 
 def my_custom_500(request, template_name='500.html'):
-
     #  500 error handler. Templates: `500.html' Context: None
     return render_to_response(template_name,
                               context_instance=RequestContext(request))
