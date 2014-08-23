67f6c467794017f6974d47eaf21d72085e453ce1
dogancankilment <dogancankilment@gmail.com>
2014-08-23 20:58:12 +0300

    added dynamic image file path into image resizer function

--- myblog/user_place/util_image_resizer.py
+++ myblog/user_place/util_image_resizer.py
@@ -1,12 +1,13 @@
-# image
 import PIL
 from PIL import Image
+from celery import shared_task
 
 
-def image_resizer():
+@shared_task
+def image_resizer(path):
     basewidth = 300
-    img = Image.open('deneme.jpg')
+    img = Image.open(path)
     wpercent = (basewidth / float(img.size[0]))
     hsize = int((float(img.size[1]) * float(wpercent)))
     img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
-    img.save('resized_image.jpg')
+    img.save(path)
