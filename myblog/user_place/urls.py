5f96d2b1fd52e0bbc78f3f444df7159bb9b13b15
dogancankilment <dogancankilment@gmail.com>
2014-08-25 02:38:13 +0300

    test_view url deleted from user_place/urls.py

--- myblog/user_place/urls.py
+++ myblog/user_place/urls.py
@@ -2,10 +2,6 @@ from django.conf.urls import patterns, url
 # from django.conf import settings
 
 urlpatterns = patterns('',
-                       url(r'^test_view$',
-                           'user_place.views.test_view',
-                           name='test_view'),
-
                        url(r'^login$',
                            'user_place.views.my_login',
                            name='my_login'),
@@ -29,4 +25,5 @@ urlpatterns = patterns('',
                        url(r'^activation/(?P<token_id>.*)',
                            'user_place.views.activation',
                            name='activation'),
+
                        )
