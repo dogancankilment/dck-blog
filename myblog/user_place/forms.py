bc984128e86cb1c97bff3c97e3660cd2e7559dbe
dogancankilment <dogancankilment@gmail.com>
2014-08-24 13:02:04 +0300

    if controll deleted and form validation error added for email.is_exists() process and clean email function added into usercreateform

--- myblog/user_place/forms.py
+++ myblog/user_place/forms.py
@@ -21,11 +21,16 @@ class UserCreateForm(UserCreationForm):
         model = User
         fields = ("username", "email", "password1", "password2")
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if email and User.objects.filter(email=email).exclude(username=username).count():
+            raise forms.ValidationError(u'Email addresses must be unique.')
+        return email
+
     def save(self, commit=True):
         user = super(UserCreateForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
-        if User.objects.filter(email=user.email):
-            return False
 
         mail_sender.delay(user.email)
         user.is_active = False
