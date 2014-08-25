c8c583983766a4a386e61423047fbe1a98dcb10d
dogancankilment <dogancankilment@gmail.com>
2014-08-25 03:25:49 +0300

    new_comment form created in main_site/forms.py

--- myblog/main_site/forms.py
+++ myblog/main_site/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
-from .models import Post
+from .models import Post, Comments
 from user_place.util_image_resizer import image_resizer
+
 
 
 class New_Post(ModelForm):
@@ -16,3 +17,15 @@ class New_Post(ModelForm):
         post.image = self.cleaned_data.get('image')
         post.save()
         image_resizer.delay(post.image.url)
+
+
+class New_Comment(ModelForm):
+    class Meta:
+        model = Comments
+        exclude = ['created_at', 'which_user']
+
+    def save(self, root, user):
+        comment = Comments(content=self.cleaned_data["name"],
+                           which_user=user,
+                           content_object=root)
+        comment.save()
