31b4e8f52eb171b39b933d55359611c4da110224
dogancankilment <dogancankilment@gmail.com>
2014-08-25 08:59:27 +0300

    task decorator removed from functions

--- myblog/main_site/util_add_comment.py
+++ myblog/main_site/util_add_comment.py
@@ -1,9 +1,7 @@
 from django.shortcuts import get_object_or_404
 from .models import Post, Comments
-from celery import shared_task
 
 
-@shared_task
 def post_comments(request, post_id):
     # this comment making to post
     current_post = get_object_or_404(Post.objects.select_related(), pk=post_id)
@@ -11,10 +9,12 @@ def post_comments(request, post_id):
     return current_post
 
 
-@shared_task
-def comments_comment(request, post_id, current_comment):
+def comments_comment(request, post_id):
     # this comment making to comment
     count = 0
+    current_comment = get_object_or_404(
+        Comments.objects.select_related(),
+        pk=post_id)
 
     current_post = current_comment.content_object
 
@@ -27,4 +27,4 @@ def comments_comment(request, post_id, current_comment):
 
     current_post = current_post.content_object  # this line find which_post
 
-    return current_post
+    return current_comment
