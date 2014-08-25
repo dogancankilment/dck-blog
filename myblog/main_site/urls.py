84cbb6a9a92ea873376d37b9a434991e93c0f7fd
dogancankilment <dogancankilment@gmail.com>
2014-08-25 14:46:24 +0300

    urls.py configured

--- myblog/main_site/urls.py
+++ myblog/main_site/urls.py
@@ -10,16 +10,16 @@ urlpatterns = patterns('',
                            'main_site.views.new_post',
                            name='new_post'),
 
-                       url(r'^single_post/(?P<id>\w+)',
-                           'main_site.views.single_post',
-                            name='single_post'),
-
                        url(r'^edit_post/(?P<id>\w+)',
                            'main_site.views.edit_post',
                             name='edit_post'),
 
+                       url(r'^single_post/(?P<id>\w+)',
+                           'main_site.views.single_post',
+                            name='single_post'),
+
                        url(r'^new_comment/(?P<post_id>\w+)/(?P<comment_id>\w+)$',
-                           'main_site.views.new_comment',
-                           name='new_comment'),
+                           'main_site.views.add_comment',
+                           name='add_comment'),
 
 )
