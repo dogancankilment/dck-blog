93033d21ad88b1ee49442d5eeb9f8f2e5773d849
dogancankilment <dogancankilment@gmail.com>
2014-08-26 19:39:12 +0300

    added new model field. Imagefield for user_profile picture

--- myblog/user_place/models.py
+++ myblog/user_place/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name="created_by_user")
     is_verified = models.BooleanField(default=False)
-    # image = models.ImageField()
+    image = models.ImageField(upload_to='static/images/user_profile/',
+                              default='static/images/user_profile/default_profile.jpg')
 
     def __unicode__(self):
         return self.user
