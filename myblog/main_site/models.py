091b91189a031b4fa3af298d194908913b7ea620
dogancankilment <dogancankilment@gmail.com>
2014-08-26 07:59:29 +0300

    content field type changed to textfield and initial data recreated

--- myblog/main_site/models.py
+++ myblog/main_site/models.py
@@ -21,7 +21,7 @@ class Post(models.Model):
 
 class Comments(models.Model):
     which_user = models.ForeignKey(User)
-    content = models.CharField(max_length=200)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     content_type = models.ForeignKey(ContentType)
