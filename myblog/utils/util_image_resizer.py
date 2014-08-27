0bcf131b21928d4018eaab4e67733b934b0d17c4
dogancankilment <dogancankilment@gmail.com>
2014-08-27 06:57:11 +0300

    resized_image's size changed

--- myblog/utils/util_image_resizer.py
+++ myblog/utils/util_image_resizer.py
@@ -5,9 +5,10 @@ from celery import shared_task
 
 @shared_task
 def image_resizer(path):
-    basewidth = 300
+    basewidth = 700
     img = Image.open(path)
     wpercent = (basewidth / float(img.size[0]))
-    hsize = int((float(img.size[1]) * float(wpercent)))
+    # hsize = int((float(img.size[1]) * float(wpercent)))
+    hsize = 300
     img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
     img.save(path)
