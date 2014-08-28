2f863599744e23dfc63a143101c74cdfc21bda3d
dogancankilment <dogancankilment@gmail.com>
2014-08-28 09:46:30 +0300

    clean email method added into user_place/forms.py

--- myblog/user_place/forms.py
+++ myblog/user_place/forms.py
@@ -44,3 +44,10 @@ class UserProfileForm(ModelForm):
     class Meta:
         model = User
         fields = ("username", "email")
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if email and User.objects.filter(email=email).exclude(username=username).count():
+            raise forms.ValidationError(u'Email addresses must be unique.')
+        return email
